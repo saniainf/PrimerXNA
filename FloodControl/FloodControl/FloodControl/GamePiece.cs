68d5db3d3d251d9c983b49bb1860b96b47300198
saniainf <saniainf@gmail.com>
2012-01-12 14:20:02 +0700

    add class

---
+++ FloodControl/FloodControl/FloodControl/GamePiece.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FloodControl
+{
+    class GamePiece
+    {
+        public static string[] PieceTypes =
+        {
+            "Left,Right",
+            "Top,Bottom",
+            "Left,Top",
+            "Top,Right",
+            "Right,Bottom",
+            "Bottom,Left",
+            "Empty"
+        };
+
+        public const int PieceHeight = 40;
+        public const int PieceWidth = 40;
+
+        public const int MaxPlayablePieceIndex = 5;
+        public const int EmptuPieceIndex = 6;
+
+        private const int textureOffsetX = 1;
+        private const int textureOffsetY = 1;
+        private const int texturePaddingX = 1;
+        private const int texturePaddingY = 1;
+
+        private string pieceType = "";
+        private string pieceSuffix = "";
+
+        public string PieceType
+        {
+            get { return pieceType; }
+        }
+
+        public string Suffix
+        {
+            get { return pieceSuffix; }
+        }
+    }
+}
