5a81a13df420a7c3a5154fa4aaf14e781769d8cd
saniainf <saniainf@gmail.com>
2012-01-12 17:58:18 +0700

    edit

--- FloodControl/FloodControl/FloodControl/GamePiece.cs
+++ FloodControl/FloodControl/FloodControl/GamePiece.cs
@@ -24,7 +24,7 @@ namespace FloodControl
         public const int PieceWidth = 40;
 
         public const int MaxPlayablePieceIndex = 5;
-        public const int EmptuPieceIndex = 6;
+        public const int EmptyPieceIndex = 6;
 
         private const int textureOffsetX = 1;
         private const int textureOffsetY = 1;
@@ -43,5 +43,101 @@ namespace FloodControl
         {
             get { return pieceSuffix; }
         }
+
+        /* конструкторы */
+        public GamePiece(string type, string suffix)
+        {
+            pieceType = type;
+            pieceSuffix = suffix;
+        }
+
+        public GamePiece(string type)
+        {
+            pieceType = type;
+            pieceSuffix = "";
+        }
+
+        /* методы */
+        public void SetPiece(string type, string suffix)
+        {
+            pieceType = type;
+            pieceSuffix = suffix;
+        }
+
+        public void SetPiece(string type)
+        {
+            SetPiece(type, "");
+        }
+
+        public void AddSuffix(string suffix)
+        {
+            if (!pieceSuffix.Contains(suffix))
+            {
+                pieceSuffix += suffix;
+            }
+        }
+
+        public void RemoveSuffix(string suffix)
+        {
+            pieceSuffix = pieceSuffix.Replace(suffix, "");
+        }
+
+        public void RotatePiece(bool clockwise)
+        {
+            switch (pieceType)
+            {
+                case "Left,Right":
+                    pieceType = "Top,Bottom";
+                    break;
+                case "Top,Bottom":
+                    pieceType = "Left,Right";
+                    break;
+                case "Left,Top":
+                    if (clockwise)
+                        pieceType = "Top,Right";
+                    else
+                        pieceType = "Bottom,Left";
+                    break;
+                case "Top,Right":
+                    if (clockwise)
+                        pieceType = "Right,Bottom";
+                    else
+                        pieceType = "Left,Top";
+                    break;
+                case "Right,Bottom":
+                    if (clockwise)
+                        pieceType = "Bottom,Left";
+                    else
+                        pieceType = "Top,Right";
+                    break;
+                case "Bottom,Left":
+                    if (clockwise)
+                        pieceType = "Left,Top";
+                    else
+                        pieceType = "Right,Bottom";
+                    break;
+                case "Empty":
+                    break;
+            }
+        }
+
+        public string[] GetOtherEnds(string startingEnd)
+        {
+            List<string> opposites = new List<string>();
+
+            foreach (string end in pieceType.Split(','))
+            {
+                if (end != startingEnd)
+                    opposites.Add(end);
+            }
+            return opposites.ToArray();
+        }
+
+        public bool HasConnector(string directions)
+        {
+            return pieceType.Contains(directions);
+        }
+
+
     }
 }
